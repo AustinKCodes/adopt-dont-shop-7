2416feb3662336d98b74c9a43654309fcc7ca0de
Cory Bretsch <corybretsch@gmail.com>
2024-07-14 10:32:42 -0500

    applications table, application_pets table, models, and tests

---
+++ app/models/application.rb
@@ -0,0 +1,10 @@
+class Application < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :description, presence: true
+  validates :pets, presence: true
+  validates :status, presence: true
+
+  has_many :application_pets
+  has_many :pets, through: :application_pets
+end
---
+++ spec/features/applications/show_spec.rb
@@ -0,0 +1,7 @@
+require "rails_helper"
+
+RSpec.describe "application show page" do
+  it "shows an applicants submitted information" do 
+    applicant = Application.create()
+  end
+end
