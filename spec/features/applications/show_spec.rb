b032b9d26c41c93aa8966def9b15ea81abd9a145
Cory Bretsch <corybretsch@gmail.com>
2024-07-18 09:15:45 -0500

    some tests

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -51,7 +51,45 @@ RSpec.describe "application show page" do
       expect(page).to have_content("Search Results")
       expect(page).to have_content("Scibby")
     end
-
-  
   end
+
+  it "has a submission option" do
+    shelter = Shelter.create!(name: "Mystery Building", city: "Irvine CA", foster_program: false, rank: 9)
+    pet1 = shelter.pets.create!(name: "Scooby", age: 2, breed: "Great Doon", adoptable: true)
+    pet2 = shelter.pets.create!(name: "Scabby", age: 2, breed: "Great Dane", adoptable: true)
+    pet3 = shelter.pets.create!(name: "Scibby", age: 2, breed: "Great Dizzle", adoptable: true)
+    application = Application.create!(name: "Debra", address: "123", description: ";lkhj", status: "In Progress")
+    ApplicationPet.create!(application: application, pet: pet1)
+    ApplicationPet.create!(application: application, pet: pet2)
+    
+    visit "/applications/#{application.id}"
+
+    within '.submit-app' do
+      expect(page).to have_content("Submit Your Application")
+      expect(page).to have_button("Submit")
+    end
+
+    within '.app-details' do
+      expect(page).to have_content(pet1.name)
+      expect(page).to have_content(pet2.name)
+    end
+  
+    within '.submit-app' do
+      click_on 'Submit'
+      expect(page).to have_no_button('Submit')
+      expect(page).not_to have_content('Submit Your Application')
+    end
+
+    within '.pet-results' do
+      expect(page).not_to have_button('Adopt this Pet')
+    end
+
+    within '.pet-search' do
+      expect(page).not_to have_button('Submit')
+    end
+
+    within '.app-details' do
+      expect(page).to have_content("Pending")
+    end
+end
 end
